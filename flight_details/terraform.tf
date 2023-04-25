740003f16dc7203c380012708d34111ef2778500
John Rothe <john.rothe@daugherty.com>
2023-04-25 14:27:54 -0400

    tag and provider updates

--- flight_details/terraform.tf
+++ flight_details/terraform.tf
@@ -1,10 +1,29 @@
+terraform {
+  #  backend "s3" {
+  #    dynamodb_table = ""
+  #  }
+
+  required_providers {
+    archive = {
+      version = "~> 2.0"
+      source  = "hashicorp/archive"
+    }
+
+    aws = {
+      version = "~> 4.0"
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-2"
 
   default_tags {
     tags = {
-      creator   = "john.rothe@daugherty.com"
-      Terraform = true
+      project_owners = ["john.rothe@daugherty.com"]
+      code_owners    = ["john.rothe@daugherty.com"]
+      deployer       = "Terraform"
     }
   }
 }
