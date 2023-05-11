d42c6e06b1a8fe546da081964521438e17fb73b2
jdr-daugherty <102676668+jdr-daugherty@users.noreply.github.com>
2023-05-11 07:50:38 -0400

    consolidated the flight related resources into a single lambda, renamed the end-points to something that makes more sense, and placed them within the /flights/ path. (#9)

---
+++ flight_details/modules/requests/main.tf
@@ -0,0 +1,58 @@
+module "arrivals_table" {
+  source     = "../../../modules/dynamodb_simple"
+  prefix     = var.prefix
+  table_name = "by-arrival"
+}
+
+module "departures_table" {
+  source     = "../../../modules/dynamodb_simple"
+  prefix     = var.prefix
+  table_name = "by-departure"
+}
+
+module "request_handler" {
+  source = "terraform-aws-modules/lambda/aws"
+  version = "~> 4.0"
+  publish = true
+
+  function_name = "${var.prefix}-${local.lambda_name}"
+  #  description   = "My awesome lambda function"
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.10"
+
+  source_path = "${var.source_path}/${local.lambda_name}/lambda_function.py"
+
+  environment_variables = {
+    ARRIVAL_TABLE = module.arrivals_table.name
+    DEPARTURE_TABLE = module.departures_table.name
+  }
+
+  cloudwatch_logs_retention_in_days = 3
+
+  allowed_triggers = {
+    APIGatewayAny = {
+      service    = "apigateway"
+      source_arn = "${var.api_execution_arn}/*/*"
+    }
+  }
+
+  attach_policy_statements = true
+  policy_statements = {
+    dynamodb = {
+      effect    = "Allow",
+      actions   = ["dynamodb:BatchGetItem", "dynamodb:GetItem"],
+      resources = [module.arrivals_table.arn, module.departures_table.arn]
+    }
+  }
+}
+
+module "api_gateway_resource" {
+  source = "../../../modules/lambda_api_gateway_resource"
+
+  rest_api_id        = var.api_id
+  parent_resource_id = var.api_parent_resource_id
+  http_method        = "GET"
+  invoke_arn         = module.request_handler.lambda_function_invoke_arn
+  parent_path       = "flights"
+  path_part          = "{request}"
+}
