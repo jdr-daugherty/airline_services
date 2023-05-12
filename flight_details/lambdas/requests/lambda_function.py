4b7426f76cb0c6fb7c2cd370cbbb02a5b51d85c5
jdr-daugherty <102676668+jdr-daugherty@users.noreply.github.com>
2023-05-12 14:29:19 -0400

    various refactoring (#12)

--- flight_details/lambdas/requests/lambda_function.py
+++ flight_details/lambdas/requests/lambda_function.py
@@ -6,110 +6,113 @@ import os
 import signal
 import sys
 
-
 ARRIVAL_PARAMS = {'tail', 'arrival_code', 'arrival_date'}
 DEPARTURE_PARAMS = {'departure_code', 'flight_number', 'departure_date'}
 
 LOGGER = logging.getLogger()
 LOGGER.setLevel(logging.INFO)
+
+ARRIVAL_TABLE_NAME = os.getenv('ARRIVAL_TABLE')
+DEPARTURE_TABLE_NAME = os.getenv('DEPARTURE_TABLE')
+
+if ARRIVAL_TABLE_NAME is None or DEPARTURE_TABLE_NAME is None:
+    raise RuntimeError("Table name(s) not defined.")
+
 try:
     CLIENT = boto3.resource('dynamodb')
-    ARRIVAL_TABLE = CLIENT.Table(os.getenv('ARRIVAL_TABLE'))
-    DEPARTURE_TABLE = CLIENT.Table(os.getenv('DEPARTURE_TABLE'))
+    ARRIVAL_TABLE = CLIENT.Table(ARRIVAL_TABLE_NAME)
+    DEPARTURE_TABLE = CLIENT.Table(DEPARTURE_TABLE_NAME)
 except Exception as ex:
     LOGGER.exception("Failed to initialize lambda")
     raise ex
 
 
 def lambda_handler(event, context) -> object:
-    path_params = event['pathParameters']
+    if is_invalid_request(event):
+        return to_invalid_response(None)
+
+    request = event['pathParameters']['request']
     query_params = event['queryStringParameters']
-
-    if path_params is None or query_params is None:
-        LOGGER.info("Invalid parameters: %s", json.dumps(event))
-        return invalid_http_response(None)
-
-    request = path_params['request']
 
     if 'by-arrival' == request:
         return by_arrival(query_params)
     elif 'by-departure' == request:
         return by_departure(query_params)
     else:
-        return invalid_http_response(path_params)
+        return to_invalid_response(request)
 
 
 def by_arrival(query_params: dict[str, str]) -> object:
-    if not valid_parameters(query_params, ARRIVAL_PARAMS):
-        LOGGER.info("Invalid parameters: %s", str(query_params))
-        return invalid_http_response(query_params)
+    if not valid_query_params(query_params, ARRIVAL_PARAMS):
+        return to_invalid_response(f"by-arrival parameters: {str(query_params)}")
 
-    key = to_arrival_key(query_params)
-    return to_http_response(key, get_from_table(ARRIVAL_TABLE, key))
+    return get_from_table(ARRIVAL_TABLE, to_arrival_key(query_params))
 
 
 def by_departure(query_params: dict[str, str]) -> object:
-    if not valid_parameters(query_params, DEPARTURE_PARAMS):
-        LOGGER.info("Invalid parameters: %s", str(query_params))
-        return invalid_http_response(query_params)
+    if not valid_query_params(query_params, DEPARTURE_PARAMS):
+        return to_invalid_response(f"by-departure parameters: {str(query_params)}")
 
-    key = to_departure_key(query_params)
-    return to_http_response(key, get_from_table(DEPARTURE_TABLE, key))
+    return get_from_table(DEPARTURE_TABLE, to_departure_key(query_params))
 
 
-def valid_parameters(parameters: dict[str, str], required: set[str]) -> bool:
+def valid_query_params(parameters: dict[str, str], required: set[str]) -> bool:
     return parameters is not None and required.issubset(parameters.keys())
 
 
-def invalid_http_response(parameters: dict[str, str] or None) -> object:
+def to_invalid_response(message: str or None) -> object:
+    LOGGER.info("Invalid request: %s", message)
+    return http_response(400, f"Invalid request: {message}")
+
+
+def to_arrival_key(params: dict[str, str]) -> dict[str, str]:
+    """ Creates a hash key for the by-arrival table.
+
+    Keyword arguments:
+        params -- the query parameters
+    """
     return {
-        "statusCode": 400,
-        "body": f"Invalid request parameters {str(parameters)}"
+        'key': f"{params['tail']}_{params['arrival_code']}_{params['arrival_date']}"
     }
 
 
-def to_arrival_key(parameters: dict[str, str]) -> dict[str, str]:
-    tail = parameters['tail']
-    arrival_code = parameters['arrival_code']
-    arrival_date = parameters['arrival_date']
+def to_departure_key(params: dict[str, str]) -> dict[str, str]:
+    """ Creates a hash key for the by-departure table.
 
+    Keyword arguments:
+        params -- the query parameters
+    """
     return {
-        'key': f'{tail}_{arrival_code}_{arrival_date}'
+        'key': f"{params['departure_code']}_{params['flight_number']}_{params['departure_date']}"
     }
 
 
-def to_departure_key(parameters: dict[str, str]) -> dict[str, str]:
-    # [Departure Code]_[Flight Number]_[Planned Departure Date Herb
-    dep_code = parameters['departure_code']
-    flight_number = parameters['flight_number']
-    dep_date = parameters['departure_date']
-
-    return {
-        'key': f'{dep_code}_{flight_number}_{dep_date}'
-    }
-
-
-def get_from_table(table, key: dict[str, str]) -> dict[str, object]:
+def get_from_table(table, key: dict[str, str]) -> object:
     try:
-        return table.get_item(Key=key)
+        return dynamodb_to_http(key, table.get_item(Key=key))
     except BotoCoreError or ClientError as error:
         LOGGER.exception("Failed to get flight from %s: %s", table.table_name, key)
         raise error
 
 
-def to_http_response(key: dict[str, str], dynamodb_response: dict[str, object]) -> object:
+def dynamodb_to_http(key: dict[str, str], dynamodb_response: dict[str, object]) -> object:
     if 'Item' in dynamodb_response:
-        LOGGER.debug("Returning flight for: %s", key)
-        return {
-            "statusCode": 200,
-            "body": json.dumps(dynamodb_response['Item'])
-        }
-    else:
-        LOGGER.debug("Inbound flight for: %s", key)
-        return {
-            "statusCode": 404,
-            "body": f"Flight not found in table {str(key)}"
-        }
+        LOGGER.debug("Returning flight: %s", key)
+        return http_response(200, json.dumps(dynamodb_response['Item']))
+
+    LOGGER.info("Flight not found: %s", key)
+    return http_response(404, f"Flight not found in table {str(key)}")
+
+
+def http_response(status_code: int, body: str) -> object:
+    return {"statusCode": status_code, "body": body}
+
+
+def is_invalid_request(event):
+    return (event is None or
+            event['queryStringParameters'] is None or
+            event['pathParameters'] is None or
+            event['pathParameters']['request'] is None)
 
 
 def exit_gracefully(signum: int, frame: object) -> None:
